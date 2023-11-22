20261cdd7737c4b2d11aba919b5e2d26a80778bc
Hodoronk <tanasa.andrei@hotmail.com>
2023-11-22 18:24:02 +0200

    fixed undefined import

--- src/playDom.js
+++ src/playDom.js
@@ -1,6 +1,37 @@
 import { shipPanel, container, mainBoard} from "./shipPlacementDom"
-import { title } from "./nameInputDom"
-import { userName } from "."
+import { playerTurn } from "./playGame"
+import { body } from "./nameInputDom"
+import { usr } from "."
+
+
+const pcBoard = document.createElement('div')
+const playerName = document.createElement('h2')
+const pcName = document.createElement('h2')
+playerName.textContent = 'Friendly waters'
+pcName.textContent = 'Enemy Waters'
+pcName.setAttribute('id', 'pc-name')
+playerName.setAttribute('id', 'player-name')
+
+
+pcBoard.setAttribute('id', 'main-board')
+let boardSize = 10
+for(let i = 0; i < boardSize* boardSize; i++) {
+    const col = Math.floor(i / boardSize)
+    const row = i % boardSize
+    const square = document.createElement('div')
+    square.classList.add('square')
+    square.setAttribute('data-info', [col, row]);
+    const dataInfo = [col, row]
+    pcBoard.appendChild(square)
+    square.addEventListener('click', () => {
+        if(playerTurn(dataInfo) === 'Ship hit'){
+            square.classList.add('hit')
+        }else{
+            square.classList.add('miss')
+        }
+    })
+}
+
 
 
 export const removeElements = () => {
@@ -9,11 +40,12 @@ export const removeElements = () => {
 }
 
 export const loading = () => {
+
     title.classList.remove('shrink')
     title.classList.add('enlarge')
     const versusText = document.createElement('h2')
     container.appendChild(versusText)
-    versusText.textContent = `${userName} vs Computer`
+    versusText.textContent = `${usr} vs Computer`
     versusText.setAttribute('id', 'vs-text')
     setTimeout(() => {
         versusText.classList.add('fade-in')
@@ -33,6 +65,10 @@ export const populateBoards = () => {
     setTimeout(() => {
 
         mainBoard.classList.remove('invisible')
+        mainBoard.classList.add('playMode')
+        container.appendChild(pcBoard)
+        body.appendChild(pcName)
+        body.appendChild(playerName)
     },3000)
 }
 
