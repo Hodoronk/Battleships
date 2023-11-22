f09b13d93d0dc8ed828b0a8d57f85930213c8403
Hodoronk <tanasa.andrei@hotmail.com>
2023-11-22 17:44:25 +0200

    changed randomIndex, no more duplicates

--- src/pcPlace.js
+++ src/pcPlace.js
@@ -1,6 +1,6 @@
 import { Gameboard } from "./gameboard";
 const ships = [5, 4, 3, 3, 1]
-const pcGameboard = new Gameboard()
+export const pcGameboard = new Gameboard()
 
 const allCoords = () => {
     let array = [];
@@ -11,49 +11,68 @@ const allCoords = () => {
     }
     return array;
 };
-
-let coords = allCoords()
-
-
 const randomOrientation = () =>{
     const orientation = Math.round(Math.random())
     return orientation
 }
-
 export const randomIndex = (len) => {
     const randomIndex = Math.floor(Math.random() * len);
     return randomIndex
 }
-
 const removeOccupiedCoords = (coords, occupied) => {
 
-    const filteredCoords = coords.filter(coord => !occupied.includes(coord));
+    const filteredCoords = coords.filter(coord => !occupied.some(o => JSON.stringify(o) === JSON.stringify(coord)));
     return filteredCoords;
 };
 
+const  hasCommonElements = (array1, array2) => {
+    return array1.some(item => array2.some(innerArray => JSON.stringify(innerArray) === JSON.stringify(item)));
+};
 
+
+
+
+let coords = allCoords();
     export const pcPlace = () => {
-        const coords = allCoords();
     
         for (let i = 0; i < ships.length ; i++) {
+
             let orientation = randomOrientation();
             let rIndex;
-            console.log(orientation)
+            let mockShip
             if (orientation === 0) {
                 do {
-                    rIndex = randomIndex(100);
-                } while (ships[i] + coords[rIndex][0] > 10);
-    
+                    rIndex = randomIndex(99 - pcGameboard.occupied.length);
+                    mockShip = pcGameboard.mockX(ships[i], coords[rIndex][0], coords[rIndex][1])
+
+
+                } while (
+                    (ships[i] + coords[rIndex][0] > 10) ||
+                    (ships[i] + coords[rIndex][1] > 10) 
+
+                    
+                );
+
+
                 pcGameboard.xPlace(ships[i], coords[rIndex][0], coords[rIndex][1]);
+
+
             } else if (orientation === 1) {
                 do {
-                    rIndex = randomIndex(100);
-                } while (ships[i] + coords[rIndex][1] > 10);
-    
+                    rIndex = randomIndex(99 - pcGameboard.occupied.length);
+                    mockShip = pcGameboard.mockY(ships[i], coords[rIndex][0], coords[rIndex][1])
+
+                } while (
+                    (ships[i] + coords[rIndex][1] > 10) ||
+                    (ships[i] + coords[rIndex][0] > 10) 
+                    );
+     
                 pcGameboard.yPlace(ships[i], coords[rIndex][0], coords[rIndex][1]);
             }
-    
-            removeOccupiedCoords(coords, pcGameboard.occupied);
+            
+            coords = removeOccupiedCoords(coords, pcGameboard.occupied);
+
         }
-        console.log(pcGameboard.occupied);
+        console.log(pcGameboard.occupied)
+        console.log(coords)
     };
