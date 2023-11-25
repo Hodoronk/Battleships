35083a7d7a493b3272dcfb24740768d74a7493e5
Hodoronk <tanasa.andrei@hotmail.com>
2023-11-25 15:43:13 +0200

    player turn logic

--- src/playGame.js
+++ src/playGame.js
@@ -1,18 +1,48 @@
-import { pcGameboard } from "./pcPlace"
+import { squareArr } from "./playDom"
 import { playerGameboard } from "./shipPlacementDom"
+import { pcGameboard } from "./pcPlace"
 
 
-export const playGame = () => {
+export const playGame = async () => {
     do{
-        playerTurn()
-        computerTurn()
-    }while(!pcGameboard.allSank || !playerGameboard.allSank)
-}
+        await playerTurn()
+        await computerTurn()
 
+    }while(pcGameboard.allSank() === false || playerGameboard.allSank() === false)
+
+}
 const playerTurn = () => {
+    return new Promise((resolve) => {
+        const squareClickHandler = (event) => {
+            const clickedSquare = event.target
+            const data = clickedSquare.getAttribute('data-info')
+            const dataInfo = data.split(',').map(Number);
 
+            if(pcGameboard.receiveAttack(dataInfo[0], dataInfo[1]) === 'hit') {
+                clickedSquare.classList.add('hit')
+
+            }else if(pcGameboard.receiveAttack(dataInfo[0], dataInfo[1]) === 'sunk'){
+                clickedSquare.classList.add('hit')
+            
+
+            }else if(pcGameboard.receiveAttack(dataInfo[0], dataInfo[1]) === 'Miss!') {
+                clickedSquare.classList.add('miss')
+            }
+
+            const indexOf = squareArr.indexOf(clickedSquare)
+            squareArr.forEach((square) => {
+                square.removeEventListener('click', squareClickHandler)
+            })
+            squareArr.splice(indexOf, 1)
+            resolve()
+        }
+        
+        squareArr.forEach((square) => {
+            square.addEventListener('click', squareClickHandler)
+        })
+
+    })
 }
-
-computerTurn = () => {
-    
+const computerTurn = async() => {
+    console.log('mock')
 }
